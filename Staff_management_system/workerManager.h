d5b4174f6956354e6946535973f127e7504e2499
Zhang Huchuan (uif25032) <huchuan.zhang@continental-corporation.com>
2023-02-03 16:22:56 +0800

    modify 20230203 all functions

--- Staff_management_system/workerManager.h
+++ Staff_management_system/workerManager.h
@@ -7,6 +7,9 @@ using namespace std;
 #include "manager.h"
 #include "boss.h"
 
+#include <fstream>
+#define FILENAME "empFile.txt"
+
 class WorkerManager
 {
 public:
@@ -26,6 +29,39 @@ public:
 
 	//增加职工
 	void Add_Emp();
+
+	//保存文件
+	void save();
+
+	//标志文件是否为空
+	bool m_FileIsEmpty;
+
+	//统计人数
+	int get_EmpNum();
+
+	//初始化员工
+	void init_Emp();
+
+	//显示职工
+	void Show_Emp();
+
+	//删除职工
+	void Del_Emp();
+
+	//按照职工编号判断职工是否存在,若存在返回职工在数组中位置，不存在返回-1
+	int IsExist(int id);
+
+	//修改职工
+	void Mod_Emp();
+
+	//查找职工
+	void Find_Emp();
+
+	//排序职工
+	void Sort_Emp();
+
+	//清空文件
+	void Clean_File();
 
 	~WorkerManager();
 
